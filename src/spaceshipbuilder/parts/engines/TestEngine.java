e7764b351a0b2d715d9b5f47531a9a6a1faa7403
kutz_865792 <kutz_865792@VG162004A8166D.leanderisd.org>
2020-01-10 13:03:01 -0600

    Added TestEngine

---
+++ src/spaceshipbuilder/parts/engines/TestEngine.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package spaceshipbuilder.parts.engines;
+import spaceshipbuilder.parts.*;
+/**
+ *
+ * @author kutz_865792
+ */
+public class TestEngine extends Engine {
+    
+    //This is a test, please ignore
+    
+    public TestEngine(int x, int y)
+    {
+        super(100, 100, x, y, 100);
+    }
+}
